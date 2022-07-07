38c62a28b376d72f7e8f5d67fe8079312564cd39
nickcale <newnewmagicwand@protonmail.com>
2022-07-07 18:41:52 -0400

    Added env variable to store secret session key

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
+# load secret key environment variable 
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+# import flask
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hasdfhasdf'
+    app.config['SECRET_KEY'] = os.getenv('APP_CONFIG_KEY')
 
     from .views import views
 
